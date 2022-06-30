81c43214338c34a6f8ef5edfd8a7299c6f504aa1
sushil <sushilwaghe2169@gmail.com>
2022-06-30 12:36:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [count, setCount]= useState(0);
+
+  function increase(){
+    setCount(count+1);
+  }
+  function decrease(){
+    setCount(count-1);
+  }
   return (
     <div className="container">
-      
+      <div className='counter-app'>
+        <button onClick={increase} className='counter-btn'>Increase</button>
+        <h2 id="counter" className='counter-heading'>{count}</h2>
+        <button onClick={decrease} className='counter-btn'>Decrease</button>
+      </div>
     </div>
   );
 }
